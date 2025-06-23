bedb440ede2595498e1c4bbe743faf576e18edc4
Filippo Avi <avi.filippo@gmail.com>
2025-06-23 17:38:20 +0200

    Event logic fix and upgrades

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ bool initializeData = true; // If true: delete and reinitialize data.csv file
 
 // Sensors ---------------------------------------------------------------------
 unsigned long sensorUpdateInterval = 5000; // 5 seconds 
-unsigned long accelerationPollInterval = 10; // 100 Hz
+unsigned long accelerationPollInterval = 100; // 10 Hz
 static float maxAccelMag = 0;
 static float accelMagnitude;
 static float maxGyroMag = 0;
@@ -31,6 +31,7 @@ static float curGyrX;
 static float curGyrY;
 static float curGyrZ;
 bool useEvents = true; // Toggle events usage
+bool bsecDebug = false;
 //------------------------------------------------------------------------------
 
 // Event memory ----------------------------------------------------------------
@@ -69,7 +70,7 @@ void setup() {
   digitalWrite(sensorCS, HIGH);
   pinMode(SD_CS_PIN, OUTPUT);
   digitalWrite(SD_CS_PIN, HIGH);
-  delay(500);
+  delay(1000);
 
   // Initialize sensors, SD card and RTC
   sdSetup();
@@ -131,10 +132,25 @@ void loop() {
                   sensorReadTemperature() + "," + sensorReadPressure() + "," +
                   sensorReadVOC() + "," + sensorReadCO2() + "," + sensorReadHumidity();
     
+    Serial.println(line);
     sdWrite(line, "data.csv");
     // So typically a line of the CSV takes more or less 80-100 bytes of memory
     // 1 reading every 5 seconds for 60 days is 100-150 megabytes
-    Serial.println(line);
+    
+
+    // DEBUG BSEC
+    if (bsecDebug) {
+      Serial.print("BSEC: VOC=");
+      Serial.print(bsec.b_voc_eq());
+      Serial.print(" CO2=");
+      Serial.print(bsec.co2_eq());
+      Serial.print(" Humidity=");
+      Serial.print(bsec.comp_h());
+      Serial.print(" Temperature=");
+      Serial.print(bsec.comp_t());
+      Serial.print(" Accuracy=");
+      Serial.println(bsec.accuracy());
+    }
 
     // Reset the maximum acceleration and rotation speed value
     maxAccelMag = 0;
@@ -182,6 +198,26 @@ void loop() {
     eventMemory[4][memoryIndex] = curGyrY;
     eventMemory[5][memoryIndex] = curGyrZ;
 
+    // DEBUG: Print the current values to the Serial Monitor
+    Serial.println("------------------------------------------------");
+    Serial.print("   Index: ");
+    Serial.print(memoryIndex);
+    Serial.print("   Time: ");
+    Serial.print(eventTime[memoryIndex]);
+    Serial.print(" Acceleration: [");
+    Serial.print(eventMemory[0][memoryIndex]);
+    Serial.print(", ");
+    Serial.print(eventMemory[1][memoryIndex]);
+    Serial.print(", ");
+    Serial.print(eventMemory[2][memoryIndex]);
+    Serial.print("] Gyroscope: [");
+    Serial.print(eventMemory[3][memoryIndex]);
+    Serial.print(", ");
+    Serial.print(eventMemory[4][memoryIndex]);
+    Serial.print(", ");
+    Serial.print(eventMemory[5][memoryIndex]);
+    Serial.println("]");
+
     // EVENT HANDLING LOGIC --------------------------------------------------------
     if(memoryIndex < eventMemorySize - 1) {
       memoryIndex++;
@@ -195,49 +231,64 @@ void loop() {
       String lines = "";
       for(int i = 0; i < eventMemorySize; i++) {
         lines += eventTime[(i + memoryIndex)%eventMemorySize] + "[past" + String(i) + "]" +
-                      String(eventMemory[0][(i + memoryIndex)%eventMemorySize]) + "," + String(eventMemory[1][(i + memoryIndex)%eventMemorySize]) + "," + 
-                      String(eventMemory[2][(i + memoryIndex)%eventMemorySize]) + "," + String(eventMemory[3][(i + memoryIndex)%eventMemorySize]) + "," + 
-                      String(eventMemory[4][(i + memoryIndex)%eventMemorySize]) + "," + String(eventMemory[5][(i + memoryIndex)%eventMemorySize]);
+                      String(eventMemory[0][(i + memoryIndex)%eventMemorySize]) + "," +
+                      String(eventMemory[1][(i + memoryIndex)%eventMemorySize]) + "," + 
+                      String(eventMemory[2][(i + memoryIndex)%eventMemorySize]) + "," +
+                      String(eventMemory[3][(i + memoryIndex)%eventMemorySize]) + "," + 
+                      String(eventMemory[4][(i + memoryIndex)%eventMemorySize]) + "," +
+                      String(eventMemory[5][(i + memoryIndex)%eventMemorySize]) + "\n";
       }
       saveBuffer += lines;
-      //sdWrite(lines, "event.csv");
-      //Serial.println("Saving data!");
-      //Serial.println(lines);
       savingEvent = true;
       eventCounter = 0;
       toSave = eventMemorySize;
     }
     // If event is detected while already saving values, keep track of more values
     else if(useEvents && (accelMagnitude > accTreshold || gyroMagnitude > gyrTreshold) && savingEvent) {
-      toSave += eventCounter;
+      toSave = eventMemorySize; // check!
     }
 
-    // FIX THIS
     // Save every eventMemorySize values to keep track of the future after the event
     if (useEvents && savingEvent && eventCounter % eventMemorySize == 0 && eventCounter != 0) {
       // Save previous eventMemorySize values before the event
       String lines = "";
       for(int i = 0; i < eventMemorySize; i++) {
-        lines += eventTime[(i + memoryIndex)%eventMemorySize] + "[future" + String(i) + "]" +
-                      String(eventMemory[0][(i + memoryIndex)%eventMemorySize]) + "," + String(eventMemory[1][(i + memoryIndex)%eventMemorySize]) + "," + 
-                      String(eventMemory[2][(i + memoryIndex)%eventMemorySize]) + "," + String(eventMemory[3][(i + memoryIndex)%eventMemorySize]) + "," + 
-                      String(eventMemory[4][(i + memoryIndex)%eventMemorySize]) + "," + String(eventMemory[5][(i + memoryIndex)%eventMemorySize]);
+        lines += eventTime[(i + memoryIndex + 1) % eventMemorySize] + "," +
+                      String(eventMemory[0][(i + memoryIndex + 1) % eventMemorySize]) + "," +
+                      String(eventMemory[1][(i + memoryIndex + 1) % eventMemorySize]) + "," + 
+                      String(eventMemory[2][(i + memoryIndex + 1) % eventMemorySize]) + "," +
+                      String(eventMemory[3][(i + memoryIndex + 1) % eventMemorySize]) + "," + 
+                      String(eventMemory[4][(i + memoryIndex + 1) % eventMemorySize]) + "," +
+                      String(eventMemory[5][(i + memoryIndex + 1) % eventMemorySize]) + "\n";
       }
       saveBuffer += lines;
-      //sdWrite(lines, "event.csv");
-      //Serial.println("Saving data!");
-      //Serial.println(lines);
     }
 
-    // FIX THIS
     // Keep track of how many values have been polled after the event
-    if(useEvents && savingEvent && eventCounter < toSave) {
+    if(useEvents && savingEvent && toSave > 0) {
+      toSave -= 1;
       eventCounter += 1;
     }
-    // FIX THIS
-    else if (useEvents && savingEvent && eventCounter > toSave-2) {
+    else if (useEvents && savingEvent && toSave <= 0) {
       savingEvent = false;
-      // We have to save what we have recorded so far, less than eventMemorySize values ... TODO
+      String lines = "";
+      // We have to save what we have recorded so far, less than eventMemorySize values
+      for(int i = - (eventCounter % eventMemorySize) + 1; i < 1; i++) {
+        lines += eventTime[(i + memoryIndex) % eventMemorySize] + "," +
+                      String(eventMemory[0][(i + memoryIndex) % eventMemorySize]) + "," +
+                      String(eventMemory[1][(i + memoryIndex) % eventMemorySize]) + "," + 
+                      String(eventMemory[2][(i + memoryIndex) % eventMemorySize]) + "," +
+                      String(eventMemory[3][(i + memoryIndex) % eventMemorySize]) + "," + 
+                      String(eventMemory[4][(i + memoryIndex) % eventMemorySize]) + "," +
+                      String(eventMemory[5][(i + memoryIndex) % eventMemorySize]) + "\n";
+      }
+      saveBuffer += lines;
+
+      sdWrite(saveBuffer, "event.csv");
+      Serial.println("Saving data!");
+      Serial.println(saveBuffer);
+
+      saveBuffer = ""; // Reset the save buffer
     }
     //------------------------------------------------------------------------------
   }
